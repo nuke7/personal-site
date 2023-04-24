21895a57c9042590098f1b900eb26b94879c5bb3
Marton Gombos <marton.gombos@yahoo.com>
2023-04-24 21:41:49 +0200

    sizing and linter fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import { Inter } from "next/font/google";
 import { getProjects } from "@/sanity/sanity-utils";
 import { Project } from "@/types/Project";
@@ -11,32 +11,31 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex min-h-screen flex-col items-center justify-evenly p-24">
-        <h1 className="text-2xl my-5">Projects from Sanity</h1>
-        {projects.map((project: Project) => (
-          <div
-            key={project._id}
-            className="p-4 my-5"
-          >
-            <p>{project.name}</p>
-            <p>
-              Created at:{" "}
-              {new Intl.DateTimeFormat("ISO", {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-              }).format(dateCreatedAt)}
-            </p>
-            <img
-              src={project.image}
-              alt="project thumbnail"
-            />
-          </div>
-        ))}
-      </div>
+      <h1 className="text-2xl my-5">Projects from Sanity</h1>
+      {projects.map((project: Project) => (
+        <div
+          key={project._id}
+          className="p-4 my-5"
+        >
+          <p>{project.name}</p>
+          <p>
+            Created at:{" "}
+            {new Intl.DateTimeFormat("ISO", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            }).format(dateCreatedAt)}
+          </p>
+          <img
+            style={{ width: "100%", maxWidth: "500px" }}
+            src={project.image}
+            alt="project thumbnail"
+          />
+        </div>
+      ))}
     </main>
   );
 }
