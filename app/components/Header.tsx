4bb5eda7d38ac7bf7783d178af6a1b9bb91201cb
Marton Gombos <marton.gombos@yahoo.com>
2023-04-29 23:32:08 +0200

    fix header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 export const Header = () => {
   return (
-    <header className="header sticky top-0">
+    <header className="header sticky top-0 z-50">
       <div className="container">
         <div className="logo">
           <Link href="/">Personal Page</Link>
