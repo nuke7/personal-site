fb03b860f0bc8c1b79c0146c2d5848c5314f59c6
Marton Gombos <marton.gombos@yahoo.com>
2023-05-04 00:42:01 +0200

    navbar fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header className="header sticky top-0 z-50">
-      <div className="container">
+      <div className="container flex items-center justify-between">
         <div className="logo">
           <Link href="/">Personal Page</Link>
         </div>
