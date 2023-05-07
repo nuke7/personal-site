19c4a6a20e914ea4c9069a59416162cfd929d357
Marton Gombos <marton.gombos@yahoo.com>
2023-05-08 00:48:57 +0200

    some styling

--- app/about/[[...index]]/page.tsx
+++ app/about/[[...index]]/page.tsx
@@ -6,14 +6,14 @@ export const metadata = {
 
 export default function AboutPage() {
   return (
-    <>
-      <h1 className="text-3xl">this is the about page</h1>
+    <div className="mx-auto px-4 sm:px-24 my-4">
+      <h1 className="text-3xl my-10">this is the about page</h1>
       <Link
         href="/"
-        className="text-xl active:text-sky-400 hover:bg-sky-700"
+        className="bg-gray-700 rounded-lg text-gray-200 font-bold p-3 pt-2 mt-2 hover:bg-blue-700"
       >
         Back to home
       </Link>
-    </>
+    </div>
   );
 }
