82a1c04e21bf6de70c5cc9dd32e4e35efd914b4b
Marton Gombos <marton.gombos@yahoo.com>
2023-05-14 12:45:12 +0200

    fix: :lipstick: full width projects on main page

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default async function Home() {
       <div>
         {projects.map((project: Project) => (
           <div key={project._id}>
-            <div className="max-w-5xl mx-auto p-4 flex flex-col sm:flex-row justify-center gap-4 bg-slate-200 dark:bg-gray-900 text-gray-700 dark:text-gray-200 rounded-lg sm:min-h-[400px]">
+            <div className="mx-auto p-4 flex flex-col sm:flex-row justify-center items-center gap-4 bg-slate-200 dark:bg-gray-900 text-gray-700 dark:text-gray-200 rounded-lg sm:min-h-[400px]">
               <div className="flex flex-col sm:max-w-56 w-full justify-center items-center">
                 <p className="text-3xl font-bold text-center mr-4">{project.name}</p>
 
@@ -51,7 +51,7 @@ export default async function Home() {
                 className="flex flex-col justify-center items-center sm:min-w-56 w-full"
               >
                 <img
-                  className="object-cover rounded-lg border border-gray-500 hover:-translate-y-1 transition-all duration-200 hover:border-2 hover:border-white mx-auto"
+                  className="object-cover rounded-lg border border-gray-500 hover:-translate-y-1 transition-all duration-200 hover:border-2 hover:border-white"
                   style={{ width: "100%", maxWidth: "500px" }}
                   src={project.image}
                   alt="project thumbnail"
