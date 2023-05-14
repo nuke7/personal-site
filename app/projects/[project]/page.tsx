362c77cd97862c9ebca5a5168f926bc6940b6def
Marton Gombos <marton.gombos@yahoo.com>
2023-05-14 13:08:32 +0200

    fix: :lipstick: fix project pages

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -11,9 +11,12 @@ const Project = async ({ params }: Props) => {
   const slug = params.project;
   const project = await getProject(slug);
   return (
-    <section className="mx-auto max-w-4xl">
-      <div className="mx-auto px-4 sm:px-24 my-4 flex gap-4 flex-col sm:flex-row justify-between items-center">
-        <p className="text-2xl font-semibold dark:text-slate-200">{project.name}</p>
+    <section
+      className="bg-fixed bg-no-repeat bg-cover bg-center w-screen h-fit min-h-screen pt-4"
+      style={{ backgroundImage: `url(${project.bgUrl})` }}
+    >
+      <div className="mx-auto px-4 sm:px-24 my-4 flex gap-4 flex-col sm:flex-row justify-between items-center max-w-4xl">
+        <p className="text-2xl font-semibold text-slate-200">{project.name}</p>
         <a
           className="bg-blue-700 rounded-lg text-center text-gray-200 font-bold p-3 pt-2 mt-2 hover:bg-blue-500"
           href={project.url}
@@ -31,7 +34,7 @@ const Project = async ({ params }: Props) => {
           View code repository
         </a>
       </div>
-      <div className="px-4 sm:px-24 my-4 dark:text-slate-100">
+      <div className="px-4 sm:px-24 mt-4 text-slate-100 max-w-4xl mx-auto">
         <PortableText value={project.content} />
         <Image
           src={project.image}
@@ -42,7 +45,7 @@ const Project = async ({ params }: Props) => {
         />
         <Link
           href="/"
-          className="bg-gray-700 rounded-lg text-gray-200 font-bold p-3 pt-2 mt-2 mb-4 hover:bg-blue-700"
+          className="bg-gray-700 rounded-lg text-gray-200 font-bold p-3 pt-2 mt-2 hover:bg-blue-700"
         >
           Back to home
         </Link>
