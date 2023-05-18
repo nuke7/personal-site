df4b83843e0ad1b7ec12f7cc55beb6be711981b2
Marton Gombos <marton.gombos@yahoo.com>
2023-05-18 22:23:57 +0200

    style: :lipstick: header style update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
         <div className="mb-2 text-center">
           <Link
             href="/"
-            className="text-center text-3xl font-bold bg-gradient-to-r from-purple-700 via-blue-300 to-pink-900 bg-clip-text text-transparent"
+            className="text-center text-4xl font-bold bg-gradient-to-r from-purple-700 via-blue-300 to-pink-900 bg-clip-text text-transparent"
           >
             Personal Page
           </Link>
@@ -15,13 +15,13 @@ export const Header = () => {
         <div className="links flex justify-center gap-4">
           <Link
             href="/about"
-            className="text-slate-200"
+            className="text-slate-200 sm:border-2 sm:border-slate-200 rounded-md px-4 flex items-center pb-1 hover:bg-slate-200 hover:text-slate-900 active:text-blue-300 active:bg-slate-700 transition-all duration-200"
           >
             About
           </Link>
           <Link
             href="/code"
-            className="text-slate-200"
+            className="text-slate-200 sm:border-2 sm:border-slate-200 rounded-md px-4 flex items-center pb-1 hover:bg-slate-200 hover:text-slate-900 active:text-blue-300 active:bg-slate-700 transition-all duration-200"
           >
             Repositories
           </Link>
